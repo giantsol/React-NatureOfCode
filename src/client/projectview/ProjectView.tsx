e334996d954d850019e684c97a8efea13edf80ba
Hansol Lee <giantsol@naver.com>
2019-05-23 21:54:17 +0900

    Sample Project01 구현해봄

--- src/client/projectview/ProjectView.tsx
+++ src/client/projectview/ProjectView.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import {RouteComponentProps} from "react-router"
+import Project01 from "./Project01"
 
-interface Props {
+interface MatchParams {
+    projectNum: string
+}
+
+interface Props extends RouteComponentProps<MatchParams> {
     socket: SocketIOClient.Emitter
 }
 
 export default class ProjectView extends React.Component<Props, any> {
     render() {
+        let main
+        switch (this.props.match.params.projectNum) {
+            case "1":
+                main = <Project01 socket={this.props.socket} />
+                break
+        }
         return (
-            <div>Hello!</div>
+            <React.Fragment>
+                { main }
+            </React.Fragment>
         )
     }
 }
